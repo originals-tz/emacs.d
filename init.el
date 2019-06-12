0e35ff7dc57be526ca7416941bc9f119c9749007
OriginalS <tengzhou989@gmail.com>
2019-06-12 19:50:02 +0800

    [Porject] : add some comment

--- init.el
+++ init.el
@@ -1,17 +1,22 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (package-initialize)
+;; package manager
 (require 'init-melpa)
 (require 'init-usepackage)
+;; vim
 (require 'init-vim)
+;; code
 (require 'init-cpp)
 (require 'init-elisp)
-(require 'init-uility)
 (require 'init-code)
+(require 'init-org)
+;; tool
+(require 'init-uility)
 (require 'init-refactor-tool)
 (require 'init-git)
+;; user
 (require 'init-modeline)
-(require 'init-org)
 (require 'init-snippet)
 (require 'init-marco)
 (require 'init-user)
