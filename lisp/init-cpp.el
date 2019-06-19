fcfd970a8359494ea5f644bca5b542b8f0d8778f
OriginalS <tengzhou989@gmail.com>
2019-06-19 15:36:11 +0800

    [cpp] : fix

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -3,10 +3,7 @@
 ;;-----
 (use-package irony
   :ensure t)
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-hook 'after-init-hook 'global-company-mode)
 
 ;;-----
 ;; company
