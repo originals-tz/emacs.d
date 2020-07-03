7b3fee888942938e9ecd7de15bfeb83d328c4150
originals <tengzhou989@icloud.com>
2020-07-03 22:35:38 +0800

    feat : C-j C-k for search

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -1,14 +1,25 @@
 ;;-----
 ;; TAGS
 ;;-----
-(use-package ctags-update
-  :ensure t)
-(add-hook 'c-mode-hook '(lambda()
-			    (add-hook 'after-save-hook 'ctags-update)
-			    ))
-(add-hook 'c++-mode-hook '(lambda()
-			    (add-hook 'after-save-hook 'ctags-update)
-			    ))
+(use-package counsel-etags
+  :ensure t
+  :bind (("C-]" . counsel-etags-find-tag-at-point))
+  :init
+  (add-hook 'prog-mode-hook
+        (lambda ()
+          (add-hook 'after-save-hook
+            'counsel-etags-virtual-update-tags 'append 'local)))
+  :config
+  (setq counsel-etags-update-interval 60)
+  (push "build" counsel-etags-ignore-directories))
+
+(add-hook 'c++-mode-hook
+      (lambda ()
+        (local-set-key (kbd "C-j") 'counsel-etags-find-tag-at-point)))
+
+(add-hook 'c++-mode-hook
+      (lambda ()
+        (local-set-key (kbd "C-k") 'counsel-etags-grep-current-directory)))
 
 ;;-----
 ;; irony
