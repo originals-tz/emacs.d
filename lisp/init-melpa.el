bd59c46a145e8232b080a6caf950dac462848407
originals <tengzhou989@icloud.com>
2020-09-19 19:55:32 +0800

    update

--- lisp/init-melpa.el
+++ lisp/init-melpa.el
@@ -1,3 +1,3 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://elpa.emacs-china.org/melpa/") t)
 (provide 'init-melpa)
